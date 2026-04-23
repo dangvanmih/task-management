9fa355c1c73cd705022fcf922d0df52fc39a8fd8
dangvanmih <minhmeo2k413@gmail.com>
2026-04-23 15:32:59 +0700

    tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.model");
 const md5 = require("md5");
-//[POST] /api/v1/users/
+//[POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
   try {
     req.body.password = md5(req.body.password);
@@ -39,4 +39,39 @@ module.exports.register = async (req, res) => {
   } catch (error) {
     res.json("không tìm thấy");
   }
+};
+
+//[POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "email không tồn tại!"
+    });
+    return
+  }
+
+  if (md5(password) != user.password) {
+    res.json({
+      code: 400,
+      message: "Mật khẩu không chính xác!"
+    });
+    return
+  }
+
+  const token = user.token;
+  res.cookie("token", token);
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công!",
+    token: token
+  });
 };
